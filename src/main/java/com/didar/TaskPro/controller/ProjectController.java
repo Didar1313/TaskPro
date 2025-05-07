71ac5742c7fc75c2aaf150a7048c131bea529d76
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-05-08 01:35:11 +0600

    Swagger tag apply for project api

--- src/main/java/com/didar/TaskPro/controller/ProjectController.java
+++ src/main/java/com/didar/TaskPro/controller/ProjectController.java
@@ -3,21 +3,25 @@ package com.didar.TaskPro.controller;
 import com.didar.TaskPro.dto.ProjectRequestDTO;
 import com.didar.TaskPro.service.Project;
 import com.didar.TaskPro.service.ProjectService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name="Project Resource", description = "Api managing projects")
 @RestController
 public class ProjectController {
     @Autowired
     private ProjectService service;
 
+    @Tag(name = "Create a project", description = "Create a new Project")
     @PostMapping("/api/projects")
     public Project create(@RequestBody ProjectRequestDTO dto) {
         return service.create(dto);
     }
 
+    @Tag(name = "Get all projects", description = "List of Projects")
     @GetMapping("/api/projects")
     public List<Project> getAll() {
         return service.getAll();
