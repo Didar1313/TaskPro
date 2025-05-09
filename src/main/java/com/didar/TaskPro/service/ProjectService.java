58e38f68df01953d2fca7c25d382476d3aa6bcef
didar-bhuiyan <didarbhuiyan1313@gmail.com>
2025-05-10 03:15:58 +0600

    Inject ProjectRepository so that we can save, get, delete, update the data easily

--- src/main/java/com/didar/TaskPro/service/ProjectService.java
+++ src/main/java/com/didar/TaskPro/service/ProjectService.java
@@ -1,6 +1,9 @@
 package com.didar.TaskPro.service;
 
 import com.didar.TaskPro.dto.ProjectRequestDTO;
+import com.didar.TaskPro.persistence.entity.Project;
+import com.didar.TaskPro.persistence.repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,24 +12,24 @@ import java.util.List;
 @Service
 public class ProjectService {
     private final List<Project> projects = new ArrayList<>();
+    @Autowired
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
 
     public Project create(ProjectRequestDTO dto) {
         Project project = new Project(dto.getName(), dto.getDescription());
-        projects.add(project);
-        return project;
+        return projectRepository.save(project);
     }
 
     public List<Project> getAll() {
-        return projects;
+        return projectRepository.findAll();
     }
 
     public Project getById(int id) {
-        for (Project project : projects) {
-            if (project.getId() == id) {
-                return project;
-            }
-        }
-        throw new IllegalArgumentException("Project not found");
+        return projectRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Result Not found"));
     }
 
 
@@ -34,12 +37,11 @@ public class ProjectService {
         Project p = getById(id);
         p.setName(dto.getName());
         p.setDescription(dto.getDescription());
-        return p;
+        return projectRepository.save(p);
     }
 
     public void delete(int id) {
-        Project p = getById(id);
-        projects.remove(p);
+        projectRepository.deleteById(id);
 
     }
 }
